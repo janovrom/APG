8714ead93c07c69572c06cd8032a5085713d1742
janovrom <roman.janovsky9@seznam.cz>
2015-12-01 23:38:18 +0100

    Added 1-transmitance for reflection.

--- APG/testapp/sgl/sgl.cpp
+++ APG/testapp/sgl/sgl.cpp
@@ -29,13 +29,13 @@
 #define REFRACTION
 
 // Defines uniform depth of field.
-//#define DEPTH_OF_FIELD
+#define DEPTH_OF_FIELD
 #ifdef DEPTH_OF_FIELD
-#define FOCAL_POINT_DIST	100.0f
+#define FOCAL_POINT_DIST	200.0f
 // This defines, how far from each other will samples be taken.
-#define BLUR_FACTOR			1
+#define BLUR_FACTOR			2
 // When sampling size set to x, we will have x*x samples.
-#define SAMPLE_SIZE_X		16
+#define SAMPLE_SIZE_X		4
 #define BLEND_FACTOR        SAMPLE_SIZE_X * SAMPLE_SIZE_X / (BLUR_FACTOR * BLUR_FACTOR)
 #endif
 
@@ -2298,9 +2298,9 @@ bool traceRay(Ray& ray, float& r, float& g, float &b, float refractIndex)
 				traceRay(ra, tmpRAdd, tmpGAdd, tmpBAdd, DEFAULT_REFR_INDEX);
 
 				
-				tmpR += clip(tmpRAdd * mP->ks);
-				tmpG += clip(tmpGAdd * mP->ks);
-				tmpB += clip(tmpBAdd * mP->ks);
+				tmpR += clip((1.0f - mP->transmitance) * tmpRAdd * mP->ks);
+				tmpG += clip((1.0f - mP->transmitance) * tmpGAdd * mP->ks);
+				tmpB += clip((1.0f - mP->transmitance) * tmpBAdd * mP->ks);
 				
 			//}
 #endif
--- APG/testapp/testapp.cpp
+++ APG/testapp/testapp.cpp
@@ -40,8 +40,8 @@
 
 #ifdef TEST4
 //#define TEST_4A
-//#define TEST_4B
-#define TEST_4C
+#define TEST_4B
+//#define TEST_4C
 #endif
 
 #ifdef TEST5
@@ -1026,7 +1026,7 @@ RayTraceScene(const char *scenename)
 				m.shine,
 				m.T,
 				m.ior);
-	printf("%f %f %f %f %f %f %f %f \n", m.col.r,m.col.g,m.col.b,m.kd,m.ks,	m.shine,m.T,m.ior);
+	//printf("%f %f %f %f %f %f %f %f \n", m.col.r,m.col.g,m.col.b,m.kd,m.ks,	m.shine,m.T,m.ior);
 	
 	/// store all polygons (converted into triangles)
 	const NFFStore::TriangleList &tlist = giter->geometry;
